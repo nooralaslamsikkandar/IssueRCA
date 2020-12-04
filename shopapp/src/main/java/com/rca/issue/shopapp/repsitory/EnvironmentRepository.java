4b50ff5e6d316bb068cd776542634f386f90dfcb
sanjai <udhayavaramban.sanjai@gmail.com>
2020-12-04 12:55:03 +0530

    repository

---
+++ shopapp/src/main/java/com/rca/issue/shopapp/repsitory/EnvironmentRepository.java
@@ -0,0 +1,7 @@
+package com.rca.issue.shopapp.repsitory;
+
+import com.rca.issue.shopapp.model.Environment_Master;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnvironmentRepository extends JpaRepository<Environment_Master, Integer> {
+}
---
+++ shopapp/src/main/java/com/rca/issue/shopapp/repsitory/StatusRepository.java
@@ -0,0 +1,7 @@
+package com.rca.issue.shopapp.repsitory;
+
+import com.rca.issue.shopapp.model.Status_Master;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusRepository extends JpaRepository<Status_Master, Integer> {
+}
